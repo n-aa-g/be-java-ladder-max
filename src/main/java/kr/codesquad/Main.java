7845497fc3c03378adf46b44b491ed03844f3608
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-18 18:47:17 +0900

    Feat(Main): 메인 메서드 수정
    만든 기능을 쓸 수 있게 메인 메서드를 변경했습니다.

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -10,10 +10,13 @@ public class Main {
     public static void main(String[] args) {
         View view = new View();
         List<String> players = view.inputValidatedPlayers();
+        List<String> result = view.inputResult();
         int height = view.inputHeight();
         LadderMaker maker = new LadderMaker();
         Ladder ladder = maker.makeRandomLadder(players.size() - 1, height);
+        List<Integer> outcome =ladder.play(players.size());
         String ladderFigure = ladder.drawLadder();
         view.printWholeFigure(players, ladderFigure);
+        view.printResult(players, outcome, result);
     }
 }
