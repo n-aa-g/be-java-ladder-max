597b4711381325d3810356a044efce41730a72a2
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-18 18:43:03 +0900

    Feat($Line): 한 라인을 탄 결과를 알려주는 기능 추가
    사다리 한줄을 탄 결과를 알려주는 기능을 추가했습니다.

--- src/main/java/kr/codesquad/domain/Line.java
+++ src/main/java/kr/codesquad/domain/Line.java
@@ -1,6 +1,8 @@
 package kr.codesquad.domain;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Line {
     private static final String RUNG = "=".repeat(5);
@@ -10,6 +12,14 @@ public class Line {
 
     public Line (List<Boolean> line) {
         this.line = line;
+    }
+
+    public void ride(List<Integer> position) {
+        IntStream.range(0, line.size()).forEach(index -> {
+            if (Boolean.TRUE.equals(line.get(index))) {
+                Collections.swap(position, index, index + 1);
+            }
+        });
     }
 
     public String drawLine() {
